071cac6d9865af47276a20ae0b850a7ddde47975
samt <sam.tlidi@hotmail.co.uk>
2021-05-20 20:13:12 +0100

    created toggle dark mode

--- script.js
+++ script.js
@@ -17,6 +17,28 @@ tabs.forEach(tab => {
     })
 })
 
+const button = document.querySelector(".btn-toggle");
+var icon = document.getElementById("icon")
+
+const moon = document.getElementsByClassName("moon")
+// console.log(moon)
+// moon.addEventListener("click", () => {
+//     document.body.classList.toggle("dark-mode");
+//     if (document.body.classList.contains("dark-mode")){
+//         moon.src = "images/sun.png";
+//     }else{
+//         moon.src = "images/moon.png";
+//     }
+    
+// });
+
+function toggleDarkLight() {
+    var body = document.getElementById("body");
+    var currentClass = body.className;
+    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+    
+}
+
 // function hello() {
 //     alert('Welcome!')
 // }
@@ -38,59 +60,59 @@ tabs.forEach(tab => {
 
 
 
-// const carouselImages = Array.from(document.getElementsByClassName("carousel-image"));
+const carouselImages = Array.from(document.getElementsByClassName("carousel-image"));
 
-// const startButton = document.querySelector(".controls-play")
-// const stopButton = document.querySelector(".controls-pause")
-// const leftButton = document.querySelector(".left")
-// const rightButton = document.querySelector(".right")
+const startButton = document.querySelector(".controls-play")
+const stopButton = document.querySelector(".controls-pause")
+const leftButton = document.querySelector(".left")
+const rightButton = document.querySelector(".right")
 
 
-// let currentImageIndex = 0;
+let currentImageIndex = 0;
 
-// function updateImageIndex(){
-//     let upperLimit = carouselImages.length - 1;
+function updateImageIndex(){
+    let upperLimit = carouselImages.length - 1;
 
-//     if (currentImageIndex === upperLimit){
-//         currentImageIndex = 0;
-//     }else {
-//         currentImageIndex++
-//     }
-// };
+    if (currentImageIndex === upperLimit){
+        currentImageIndex = 0;
+    }else {
+        currentImageIndex++
+    }
+};
 
-// function updateVisibilities(){
-//     updateImageIndex()
-//     carouselImages.forEach((element, index)=>{
-//         if (index === currentImageIndex) {
-//             element.classList.remove("img-hidden");
-//             element.classList.add("img-visible");
-//         } else {
-//             element.classList.remove("img-visible");
-//             element.classList.add("img-hidden");
+function updateVisibilities(){
+    updateImageIndex()
+    carouselImages.forEach((element, index)=>{
+        if (index === currentImageIndex) {
+            element.classList.remove("img-hidden");
+            element.classList.add("img-visible");
+        } else {
+            element.classList.remove("img-visible");
+            element.classList.add("img-hidden");
         
-//         }
-//     })
-// }
+        }
+    })
+}
 
-// let carouselInterval;
+let carouselInterval;
 
-// const startRunning = () => {
-//     carouselInterval = setInterval(updateVisibilities, 2500);
-// }
+const startRunning = () => {
+    carouselInterval = setInterval(updateVisibilities, 2500);
+}
 
-// const stopRunning = () => {
-//     clearInterval(carouselInterval)
-// }
+const stopRunning = () => {
+    clearInterval(carouselInterval)
+}
 
-// startButton.addEventListener("click", startRunning);
+startButton.addEventListener("click", startRunning);
 
-// stopButton.addEventListener("click", stopRunning);
+stopButton.addEventListener("click", stopRunning);
 
 
-// const pressLeft = () => {
-//     currentImageIndex = 0
-// }
+const pressLeft = () => {
+    currentImageIndex = 0
+}
 
-// leftButton.addEventListener("click", updateImageIndex);
+leftButton.addEventListener("click", updateImageIndex);
 
 
