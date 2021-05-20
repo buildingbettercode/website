75d928f5b7c1099848747fb9236925357533e322
samt <sam.tlidi@hotmail.co.uk>
2021-05-20 18:00:11 +0100

    created tabs made the tabs function

--- script.js
+++ script.js
@@ -1,54 +1,96 @@
-const carouselImages = Array.from(document.getElementsByClassName("carousel-image"));
+console.log("hello")
 
-const startButton = document.querySelector(".controls-play")
-const stopButton = document.querySelector(".controls-pause")
-const leftButton = document.querySelector(".left")
-const rightButton = document.querySelector(".right")
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
 
-
-let currentImageIndex = 0;
-
-function updateImageIndex(){
-    let upperLimit = carouselImages.length - 1;
-
-    if (currentImageIndex === upperLimit){
-        currentImageIndex = 0;
-    }else {
-        currentImageIndex++
-    }
-};
-
-function updateVisibilities(){
-    updateImageIndex()
-    carouselImages.forEach((element, index)=>{
-        if (index === currentImageIndex) {
-            element.classList.remove("img-hidden");
-            element.classList.add("img-visible");
-        } else {
-            element.classList.remove("img-visible");
-            element.classList.add("img-hidden");
-        
-        }
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
     })
-}
+})
 
-let carouselInterval;
+// function hello() {
+//     alert('Welcome!')
+// }
+// hello()
 
-const startRunning = () => {
-    carouselInterval = setInterval(updateVisibilities, 2500);
-}
-
-const stopRunning = () => {
-    clearInterval(carouselInterval)
-}
-
-    startButton.addEventListener("click", startRunning);
-
-stopButton.addEventListener("click", stopRunning);
+// console.log(hello)
 
 
-const pressLeft = () => {
-    currentImageIndex = 0
-}
 
-leftButton.addEventListener("click", pressLeft)
+
+
+
+
+
+
+
+
+
+
+
+
+// const carouselImages = Array.from(document.getElementsByClassName("carousel-image"));
+
+// const startButton = document.querySelector(".controls-play")
+// const stopButton = document.querySelector(".controls-pause")
+// const leftButton = document.querySelector(".left")
+// const rightButton = document.querySelector(".right")
+
+
+// let currentImageIndex = 0;
+
+// function updateImageIndex(){
+//     let upperLimit = carouselImages.length - 1;
+
+//     if (currentImageIndex === upperLimit){
+//         currentImageIndex = 0;
+//     }else {
+//         currentImageIndex++
+//     }
+// };
+
+// function updateVisibilities(){
+//     updateImageIndex()
+//     carouselImages.forEach((element, index)=>{
+//         if (index === currentImageIndex) {
+//             element.classList.remove("img-hidden");
+//             element.classList.add("img-visible");
+//         } else {
+//             element.classList.remove("img-visible");
+//             element.classList.add("img-hidden");
+        
+//         }
+//     })
+// }
+
+// let carouselInterval;
+
+// const startRunning = () => {
+//     carouselInterval = setInterval(updateVisibilities, 2500);
+// }
+
+// const stopRunning = () => {
+//     clearInterval(carouselInterval)
+// }
+
+// startButton.addEventListener("click", startRunning);
+
+// stopButton.addEventListener("click", stopRunning);
+
+
+// const pressLeft = () => {
+//     currentImageIndex = 0
+// }
+
+// leftButton.addEventListener("click", updateImageIndex);
+
+
