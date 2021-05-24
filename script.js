c88f73869d54546d4c6c548e792d27746f26f9d4
samt <sam.tlidi@hotmail.co.uk>
2021-05-24 23:59:13 +0100

    fixed theme and responsive menu bar

--- script.js
+++ script.js
@@ -81,9 +81,9 @@ themeButton.addEventListener('click', () => {
 // })
 
 
-// function myFunction() {
-//     alert("Thank you, your message has been received!");
-// }
+function myFunction() {
+    alert("Thank you, your message has been received!");
+}
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
@@ -91,3 +91,30 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+}
