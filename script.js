e5dae62f273ea7b5eaa92f9caf32868f362999c1
samt <sam.tlidi@hotmail.co.uk>
2021-05-26 10:53:32 +0100

    added pause/play to carousel

--- script.js
+++ script.js
@@ -88,36 +88,46 @@ function myFunction() {
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+// slideshow 
+var playing = true;
+var pauseButton = document.getElementById('pause');
+var myIndex = 0;
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
-// 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function carousel() {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
     }
-
-    slides[slideIndex - 1].style.display = "block";
-
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
 }
 
+
+function pauseSlideshow() {
+    pauseButton.innerHTML = 'Play';
+    playing = false;
+    clearInterval(slideInterval);
+}
+
+function playSlideshow() {
+    pauseButton.innerHTML = 'Pause';
+    playing = true;
+    slideInterval = setInterval(carousel, 2000);
+}
+
+pauseButton.onclick = function () {
+    if (playing) {
+        pauseSlideshow();
+    }
+    else {
+        playSlideshow();
+    }
+};
+
+carousel();
+playSlideshow();
 // 
