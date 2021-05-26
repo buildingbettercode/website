8af06216d15edc5d78e12bc77b2181e0209c49c4
samt <sam.tlidi@hotmail.co.uk>
2021-05-26 11:24:01 +0100

    fixed slideshow

--- script.js
+++ script.js
@@ -88,7 +88,11 @@ function myFunction() {
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-// slideshow 
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+// 
+// slideshow
 var playing = true;
 var pauseButton = document.getElementById('pause');
 var myIndex = 0;
@@ -127,6 +131,35 @@ pauseButton.onclick = function () {
         playSlideshow();
     }
 };
+// 
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+}
+
 
 carousel();
 playSlideshow();
